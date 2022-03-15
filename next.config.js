c4be33361bae1e0999d0cf1f2fc7178e13925727
talentedev <webdev124@outlook.com>
2022-03-16 04:03:04 +0900

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
+const withPlugins = require("next-compose-plugins");
+
+/** eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require("next-transpile-modules")([
+  "@solana/wallet-adapter-base",
+  "@solana/wallet-adapter-phantom",
+  "@solana/wallet-adapter-react",
+  "@solana/wallet-adapter-solflare",
+  "@solana/wallet-adapter-wallets",
+]);
+
+const plugins = [
+  [
+    withTM,
+    {
+      webpack5: true,
+      reactStrictMode: true,
+    },
+  ],
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    INFURA_ID: process.env.INFURA_ID,
+    ETHEREUM_RPC: process.env.ETHEREUM_RPC,
+    BSC_RPC: process.env.BSC_RPC,
+    POLYGON_RPC: process.env.POLYGON_RPC,
+    FTM_RPC: process.env.FTM_RPC
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(plugins, nextConfig)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from "next/dynamic"
+import { useMemo } from "react"
+import { ConnectionProvider } from "@solana/wallet-adapter-react"
+import { clusterApiUrl } from "@solana/web3.js"
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
+
+const SOLANA_NETWORK = WalletAdapterNetwork.Mainnet
+const network = SOLANA_NETWORK
+
+const WalletProvider = dynamic(
+  () => import("../contexts/ClientWalletProvider"),
+  {
+    ssr: false,
+  }
+)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const endpoint = useMemo(() => clusterApiUrl(network), [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider>
+        <Component {...pageProps} />
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
 
 export default MyApp
